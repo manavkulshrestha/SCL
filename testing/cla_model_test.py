Removed normalization of features which gives better performance and also fixes loader vs on the fly bug

testing/cla_model_test.py
@@ -4,6 +4,7 @@ import os.path as osp
 import torch
 
 from nn.Network import ObjectNet
+from utility import tid_name, visualize, make_pcd, load_model
 
 RP_ROOT = osp.abspath('/home/mk/rp/')
 DDPATH = osp.join(RP_ROOT, 'data/dep_data/')
@@ -47,11 +48,14 @@ def tid_colors(typeidx):
 
 
 def main():
-    cla_net = ObjectNet().cuda()
-    mp = osp.join(MODELS_PATH, 'cn_model-200.pt')
-    # mp = '/home/mk/rp/models/cn_test_model-20.pt'
-    cla_net.load_state_dict(torch.load(mp))
-    cla_net.cuda()
+    # cla_net = ObjectNet().cuda()
+    # mp = osp.join(MODELS_PATH, 'cn_model-200.pt')
+    # # mp = '/home/mk/rp/models/cn_test_model-20.pt'
+    # cla_net.load_state_dict(torch.load(mp))
+    # cla_net.cuda()
+    # cla_net.eval()
+    # cla_net = load_model(ObjectNet, 'cn_test_best_model.pt')
+    cla_net = load_model(ObjectNet, 'cn_model-200.pt')
     cla_net.eval()
 
     rp_root = osp.abspath('/home/mk/rp')
@@ -59,7 +63,7 @@ def main():
     pcd_root = osp.join(rp_root, 'data/pcd_data/')
     dep_root = osp.join(rp_root, 'data/dep_data/')
 
-    i = 1000
+    i = 0
 
     file_name = f'{i // 1000}_{i % 1000}.npz'
     pcd_file = np.load(osp.join(pcd_root, file_name))
@@ -67,22 +71,19 @@ def main():
     pcds, o_ids, t_ids = [pcd_file[x] for x in ['pc', 'oid', 'tid']]
     node_ids, dep_g = [dep_file[x] for x in ['node_ids', 'depg']]
 
-    types = ['cube', 'cylinder', 'ccuboid', 'scuboid', 'tcuboid', 'roof', 'pyramid', 'cuboid']
     oid_tid = dict(zip(o_ids, t_ids))
-    tid_name = dict(enumerate(types, start=1))
-    name = lambda oid: tid_name[oid_tid[oid]]
 
     for nid in np.unique(o_ids).astype(int):
         obj_pcd = pcds[o_ids == nid]
 
-        # pos = torch.tensor(sample_exact(normalize(obj_pcd), 512)).cuda().float()
-        # model_input = Data(pos=pos)
+        # pred_tid = cla_net.predict(obj_pcd)
+        pred_tid1, embed = cla_net.embed(obj_pcd, get_pred=True)
+        pred_tid2 = cla_net.predict_fromfeatures(embed, max_ax=1)
+        tid = oid_tid[nid]
 
-        # outs = cla_net(model_input, nobatch=True)
-        # pred_tid = outs.max(1)[1].item() + 1
-        pred_tid = cla_net.predict(obj_pcd)
-        print(f'{nid} is a {name(nid)}, predicted: {tid_name[pred_tid]}')
-    # visualize(make_pcd(pcds, colors=tid_colors(t_ids)))
+        print(f'{nid} is a {tid_name(tid)}, predicted: {tid_name(pred_tid2)}')
+    visualize(make_pcd(pcds, colors=tid_colors(t_ids)))
+
 
 if __name__ == '__main__':
     main()

testing/dep_model_test.py
@@ -80,11 +80,16 @@ def vis_test(model, loader, thresh=0.5):
 
         pred_adj = np.zeros((data.num_nodes, data.num_nodes))
         pred_e_idx = data.all_e_idx[:, pred.bool()].cpu().numpy()
-        pred_adj[tuple(pred_e_idx)] = 1
+
+        pred_adj[tuple(data.all_e_idx.cpu().numpy())] = outs.cpu().numpy()
 
         def adj_vis(i, pred_adj, adj_mat):
+
+            pred_adj_ones = np.zeros_like(pred_adj)
+            pred_adj_ones[pred_adj > 0.5] = 1
+
             fig, axes = plt.subplots(1, 2)
-            visualize_graph(pred_adj, *scene_graph_info(i), ax=axes[0])
+            visualize_graph(pred_adj_ones, *scene_graph_info(i), ax=axes[0])
             visualize_graph(adj_mat, *scene_graph_info(i), ax=axes[1])
             axes[0].title.set_text('Predicted')
             axes[1].title.set_text('Ground Truth')
@@ -106,7 +111,7 @@ def main():
     feat_net.eval()
     train_loader, val_loader, test_loader = get_depdataloaders(feat_net)
 
-    dep_net = load_model(DNet, 'dnT_best_model_GAT16.pt',
+    dep_net = load_model(DNet, 'dnT_best_model.pt',
                          model_args=[511, 256, 128], model_kwargs={'heads': 16, 'concat':False})
     # dep_net = load_model(DNet, 'dnT_best_model_fixnosam_gat8noconcat.pt', model_args=[511, 256, 128])
     dep_net.eval()