Offset vector rotation added for correct placement

robot/robot.py
@@ -37,18 +37,19 @@ class UR5:
         self.ee.release()
         self.move_timestep = 1/240
 
-    def move_ee(self, pos, orn=None, error_thresh=0.01, max_iter=100, conv_eps=0.0001, break_cond=lambda: False):
+    def move_ee(self, pos, orn=None, error_thresh=0.01, break_cond=lambda: False, max_iter=300, **kwargs):
         errors = []
-
         i = 0
+
         while True:
             q_tar = np.array(p.calculateInverseKinematics(self.id, self.ee_id, pos, targetOrientation=orn))
             q_cur = np.array([q_state[0] for q_state in p .getJointStates(self.id, self.joints)])
             err = np.linalg.norm(q_tar - q_cur)
 
+            # print(err)
             errors.append(err)
-            if break_cond() or err < error_thresh or (i > max_iter and check_convergence(errors[-5:], eps=conv_eps)):
-                print(err, 'break_cond:', break_cond())
+            if break_cond() or err < error_thresh or (i > max_iter and check_convergence(errors[-10:])):
+                print(f'final error: {err}, bc={break_cond()}')
                 break
 
             p.setJointMotorControlArray(bodyUniqueId=self.id,
@@ -62,20 +63,17 @@ class UR5:
 
         return q_tar, q_cur
 
-    def move_ee_down(self, pos, orn=(0, 0, 0, 1)):
+    def move_ee_down(self, pos, orn=(0, 0, 0, 1), **kwargs):
         """
         moves down from `pos` to z=0 until it detects object
         returns: pose=(pos, orn) at which it detected contact"""
         pos = [*pos[:2], 0]
-        # ee_tip dynamics = (0.0, 0.5, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.7071067811882787, -0.7071067811848163, 7.31230107716731e-14, -7.312301077203115e-14), 0.0, 0.0, 0.0, -1.0, -1.0, 2, 0.001)
-        # my ee tip dynamics = (0.0, 0.5, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.7071067811882787, -0.7071067811848163, 7.31230107716731e-14, -7.312301077203115e-14), 0.0, 0.0, 0.0, -1.0, -1.0, 2, 0.001)
-
-        self.move_ee(pos, orn=orn, break_cond=lambda: self.ee.detect_contact())
+        self.move_ee(pos, orn=orn, break_cond=lambda: self.ee.detect_contact(), **kwargs)
         return self.ee_pose
 
-    def move_ee_above(self, pos, orn=(0, 0, 0, 1), above_offt=(0, 0, 0.2)):
+    def move_ee_above(self, pos, orn=(0, 0, 0, 1), above_offt=(0, 0, 0.2), **kwargs):
         a_pos = np.add(pos, above_offt)
-        self.move_ee(a_pos, orn=orn)
+        self.move_ee(a_pos, orn=orn, **kwargs)
 
     def suction(self, on):
         if on:

rp.py
@@ -10,6 +10,7 @@ import os.path as osp
 import torch
 from torch_geometric.data import Data
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 from Datasets.dutility import PDPATH, DDPATH, get_depdataloaders
 from Generation.gen_lib import simulate_scene_pc, Camera, PBObjectLoader
@@ -56,7 +57,7 @@ def setup_field(loader_target, slow=False):
             pos, orn = loader_target.obj_poses[oid]
 
             c_oid = loader2.load_obj(otype=typ, pos=(xpos, ypos, 0.01), quat=orn, wait=100, slow=slow)
-            p.changeDynamics(c_oid, -1, mass=0.1)
+            p.changeDynamics(c_oid, -1, mass=0.05)
             idx += 1
 
     return loader2
@@ -185,7 +186,8 @@ def get_suc_point(pcds, oids, oid, epsilon=0.00001):
 
 def main():
     # seed = 1369 or np.random.randint(0, 10000)
-    seed = 500 or np.random.randint(0, 10000)  # 4978
+    # seed = 500 or np.random.randint(0, 10000)  # 4978
+    seed = 9457 or np.random.randint(0, 10000)  # 4978
     print(f'SEED: {seed}')
     np.random.seed(seed)
 
@@ -232,6 +234,7 @@ def main():
     robot.move_timestep = 1/240
 
     # rearrangement
+    # TODO check why cylinder is not, confirm orn stuff, build metric suite, run test set for metric
     for layer in topo_layers:
         for obj_idx in layer:
             c_oid = curr_state.obj_ids[obj_idx]
@@ -256,17 +259,20 @@ def main():
             robot.suction(True)
             robot.move_ee_above(c_pos, orn=(0, 0, 0, 1))
 
-            # obtain goal position
+            # obtain goal pose
             succ_offt = np.subtract(c_pos_from, c_pos_cen)
-            g_pos_to = g_pos_cen+succ_offt
+            g_orn_mat = R.from_quat(g_orn_to).as_matrix()
+            g_pos_to = g_pos_cen+(g_orn_mat@succ_offt)
 
             # g_orn_to = (0, 0, 0, 1)
             # move above goal position, move to goal, drop, move above goal
             robot.move_ee_above(g_pos_to, orn=g_orn_to)
-            robot.move_ee(g_pos_to+[0, 0, 0.0], orn=g_orn_to)
-            for _ in range(1000):
-                p.stepSimulation()
+            robot.move_ee(g_pos_to+[0, 0, 0.005], orn=g_orn_to)
             robot.suction(False)
+
+            for _ in range(500):
+                p.stepSimulation()
+
             robot.move_ee_above(g_pos_cen, orn=(0, 0, 0, 1))
 
     while True: